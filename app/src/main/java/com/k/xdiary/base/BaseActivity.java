0621808cd4ebcc5568ec0e6b424f6a7f6cd9a28d
wudifamo <125282834@qq.com>
2016-12-20 00:35:08 +0800

    2016年12月20日00:34:59

--- app/src/main/java/com/k/xdiary/base/BaseActivity.java
+++ app/src/main/java/com/k/xdiary/base/BaseActivity.java
@@ -9,12 +9,16 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.google.gson.Gson;
 import com.k.xdiary.R;
@@ -24,225 +28,272 @@ import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
 
 public abstract class BaseActivity extends SwipeBackActivity implements View.OnClickListener {
-	/**
-	 * 是否沉浸状态栏
-	 **/
-	private boolean isSetStatusBar = true;
-	/**
-	 * 是否允许全屏
-	 **/
-	private boolean mAllowFullScreen = false;
-	/**
-	 * 是否禁止旋转屏幕
-	 **/
-	private boolean isAllowScreenRoate = true;
-	/**
-	 * 当前Activity渲染的视图View
-	 **/
-	private View mContextView = null;
-	/**
-	 * 是否输出日志信息
-	 **/
-	private boolean isDebug;
-	private String APP_NAME;
-	protected final String TAG = this.getClass().getSimpleName();
-	protected SwipeBackLayout mSwipeBackLayout;
-	protected Context mContext;
-	protected Gson gson = new Gson();
-	protected Toolbar mToolBar;
+    /**
+     * 是否沉浸状态栏
+     **/
+    private boolean isSetStatusBar = true;
+    /**
+     * 是否允许全屏
+     **/
+    private boolean mAllowFullScreen = false;
+    /**
+     * 是否禁止旋转屏幕
+     **/
+    private boolean isAllowScreenRoate = true;
+    /**
+     * 当前Activity渲染的视图View
+     **/
+    private View mContextView = null;
+    /**
+     * 是否输出日志信息
+     **/
+    private boolean isDebug;
+    private String APP_NAME;
+    protected final String TAG = this.getClass().getSimpleName();
+    protected SwipeBackLayout mSwipeBackLayout;
+    protected Context mContext;
+    protected Gson gson = new Gson();
+    protected Toolbar mToolBar;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 //		isDebug = MApplication.isDebug;
 //		APP_NAME = MApplication.APP_NAME;
-		$Log(TAG + "-->onCreate()");
-		try {
-			mContext = this;
-			Bundle bundle = getIntent().getExtras();
-			initParms(bundle);
-			mContextView = LayoutInflater.from(this).inflate(bindLayout(), null);
-			if (mAllowFullScreen) {
-				this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-				requestWindowFeature(Window.FEATURE_NO_TITLE);
-			}
-			if (isSetStatusBar) {
-				steepStatusBar();
-			}
-			setContentView(mContextView);
-			mToolBar = (Toolbar) findViewById(R.id.toolbar);
-			if (mToolBar != null) {
-				setSupportActionBar(mToolBar);
-			}
-			if (!isAllowScreenRoate) {
-				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-			} else {
-				setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-			}
-			mSwipeBackLayout = getSwipeBackLayout();
-			mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
-			mSwipeBackLayout.addSwipeListener(new SwipeBackLayout.SwipeListener() {
-				@Override
-				public void onScrollStateChange(int state, float scrollPercent) {
+        $Log(TAG + "-->onCreate()");
+        try {
+            mContext = this;
+            Bundle bundle = getIntent().getExtras();
+            initParms(bundle);
+            mContextView = LayoutInflater.from(this).inflate(bindLayout(), null);
+            if (mAllowFullScreen) {
+                this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                requestWindowFeature(Window.FEATURE_NO_TITLE);
+            }
+            if (isSetStatusBar) {
+                steepStatusBar();
+            }
+            setContentView(mContextView);
+            mToolBar = (Toolbar) findViewById(R.id.toolbar);
+            if (mToolBar != null) {
+                setSupportActionBar(mToolBar);
+            }
+            if (!isAllowScreenRoate) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            } else {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+            mSwipeBackLayout = getSwipeBackLayout();
+            mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
+            mSwipeBackLayout.addSwipeListener(new SwipeBackLayout.SwipeListener() {
+                @Override
+                public void onScrollStateChange(int state, float scrollPercent) {
 
-				}
+                }
 
-				@Override
-				public void onEdgeTouch(int edgeFlag) {
-				}
+                @Override
+                public void onEdgeTouch(int edgeFlag) {
+                }
 
-				@Override
-				public void onScrollOverThreshold() {
-				}
-			});
-			initView(mContextView);
-			doBusiness();
+                @Override
+                public void onScrollOverThreshold() {
+                }
+            });
+            initView(mContextView);
+            doBusiness();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * [沉浸状态栏]
-	 */
-	private void steepStatusBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			// 透明状态栏
-			getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-			// 透明导航栏
-			getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-		}
-	}
+    /**
+     * [沉浸状态栏]
+     */
+    private void steepStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
 
-	/**
-	 * [初始化Bundle参数]
-	 */
-	public abstract void initParms(Bundle parms);
+    /**
+     * [初始化Bundle参数]
+     */
+    public abstract void initParms(Bundle parms);
 
-	/**
-	 * [绑定布局]
-	 */
-	public abstract int bindLayout();
+    /**
+     * [绑定布局]
+     */
+    public abstract int bindLayout();
 
-	/**
-	 * [重写： 1.是否沉浸状态栏 2.是否全屏 3.是否禁止旋转屏幕]
-	 */
-	// public abstract void setActivityPre();
+    /**
+     * [重写： 1.是否沉浸状态栏 2.是否全屏 3.是否禁止旋转屏幕]
+     */
+    // public abstract void setActivityPre();
 
-	/**
-	 * [初始化控件]
-	 */
-	public abstract void initView(final View view);
+    /**
+     * [初始化控件]
+     */
+    public abstract void initView(final View view);
 
-	/**
-	 * [业务操作]
-	 */
-	public abstract void doBusiness();
+    /**
+     * [业务操作]
+     */
+    public abstract void doBusiness();
 
-	/**
-	 * View点击
-	 **/
-	public abstract void widgetClick(View v);
+    /**
+     * View点击
+     **/
+    public abstract void widgetClick(View v);
 
-	@Override
-	public void onClick(View v) {
-		if (fastClick())
-			widgetClick(v);
-	}
+    @Override
+    public void onClick(View v) {
+        if (fastClick())
+            widgetClick(v);
+    }
 
-	/**
-	 * [页面跳转]
-	 */
-	public void startActivity(Class<?> clz) {
-		startActivity(clz, null);
-	}
+    /**
+     * [页面跳转]
+     */
+    public void startActivity(Class<?> clz) {
+        startActivity(clz, null);
+    }
 
-	/**
-	 * [携带数据的页面跳转]
-	 */
-	public void startActivity(Class<?> clz, Bundle bundle) {
-		Intent intent = new Intent();
-		intent.setClass(this, clz);
-		if (bundle != null) {
-			intent.putExtras(bundle);
-		}
-		startActivity(intent);
-	}
+    /**
+     * [携带数据的页面跳转]
+     */
+    public void startActivity(Class<?> clz, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(this, clz);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+    }
 
-	@SuppressWarnings("unchecked")
-	public <T extends View> T $(int resId) {
-		return (T) super.findViewById(resId);
-	}
+    @SuppressWarnings("unchecked")
+    public <T extends View> T $(int resId) {
+        return (T) super.findViewById(resId);
+    }
 
-	/**
-	 * [含有Bundle通过Class打开编辑界面]
-	 */
-	public void startActivityForResult(Class<?> cls, Bundle bundle,
-									   int requestCode) {
-		Intent intent = new Intent();
-		intent.setClass(this, cls);
-		if (bundle != null) {
-			intent.putExtras(bundle);
-		}
-		startActivityForResult(intent, requestCode);
-	}
+    /**
+     * [含有Bundle通过Class打开编辑界面]
+     */
+    public void startActivityForResult(Class<?> cls, Bundle bundle,
+                                       int requestCode) {
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivityForResult(intent, requestCode);
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		$Log(TAG + "--->onResume()");
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        $Log(TAG + "--->onResume()");
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		$Log(TAG + "--->onDestroy()");
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        $Log(TAG + "--->onDestroy()");
+    }
 
-	/**
-	 * [是否允许全屏]
-	 */
-	public void setAllowFullScreen(boolean allowFullScreen) {
-		this.mAllowFullScreen = allowFullScreen;
-	}
+    /**
+     * [是否允许全屏]
+     */
+    public void setAllowFullScreen(boolean allowFullScreen) {
+        this.mAllowFullScreen = allowFullScreen;
+    }
 
-	/**
-	 * [是否设置沉浸状态栏]
-	 */
-	public void setSteepStatusBar(boolean isSetStatusBar) {
-		this.isSetStatusBar = isSetStatusBar;
-	}
+    /**
+     * [是否设置沉浸状态栏]
+     */
+    public void setSteepStatusBar(boolean isSetStatusBar) {
+        this.isSetStatusBar = isSetStatusBar;
+    }
 
-	/**
-	 * [是否允许屏幕旋转]
-	 *
-	 * @param isAllowScreenRoate
-	 */
-	public void setScreenRoate(boolean isAllowScreenRoate) {
-		this.isAllowScreenRoate = isAllowScreenRoate;
-	}
+    /**
+     * [是否允许屏幕旋转]
+     *
+     * @param isAllowScreenRoate
+     */
+    public void setScreenRoate(boolean isAllowScreenRoate) {
+        this.isAllowScreenRoate = isAllowScreenRoate;
+    }
 
-	/**
-	 * [日志输出]
-	 *
-	 * @param msg
-	 */
-	protected void $Log(String msg) {
-		if (isDebug) {
-			Log.d(APP_NAME, msg);
-		}
-	}
+    /**
+     * [日志输出]
+     *
+     * @param msg
+     */
+    protected void $Log(String msg) {
+        if (isDebug) {
+            Log.d(APP_NAME, msg);
+        }
+    }
 
-	/**
-	 * [防止快速点击]
-	 */
-	private boolean fastClick() {
-		long lastClick = 0;
-		if (System.currentTimeMillis() - lastClick <= 1000) {
-			return false;
-		}
-		lastClick = System.currentTimeMillis();
-		return true;
-	}
+    /**
+     * [防止快速点击]
+     */
+    private boolean fastClick() {
+        long lastClick = 0;
+        if (System.currentTimeMillis() - lastClick <= 1000) {
+            return false;
+        }
+        lastClick = System.currentTimeMillis();
+        return true;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideKeyboard(v, ev)) {
+                hideKeyboard(v.getWindowToken());
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * 根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘，因为当用户点击EditText时则不能隐藏
+     *
+     * @param v
+     * @param event
+     * @return
+     */
+    private boolean isShouldHideKeyboard(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] l = {0, 0};
+            v.getLocationInWindow(l);
+            int left = l[0], top = l[1], bottom = top + v.getHeight(), right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击EditText的事件，忽略它。
+                return false;
+            } else {
+                return true;
+            }
+        }
+        // 如果焦点不是EditText则忽略，这个发生在视图刚绘制完，第一个焦点不在EditText上，和用户用轨迹球选择其他的焦点
+        return false;
+    }
+
+    /**
+     * 获取InputMethodManager，隐藏软键盘
+     *
+     * @param token
+     */
+    private void hideKeyboard(IBinder token) {
+        if (token != null) {
+            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
 }
--- app/src/main/java/com/k/xdiary/ui/weight/AddWeightActivity.java
+++ app/src/main/java/com/k/xdiary/ui/weight/AddWeightActivity.java
@@ -3,6 +3,7 @@ package com.k.xdiary.ui.weight;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.k.xdiary.R;
 import com.k.xdiary.base.BaseActivity;
@@ -12,53 +13,54 @@ import java.util.Calendar;
 
 public class AddWeightActivity extends BaseActivity implements DatePickerDialog.OnDateSetListener {
 
-	private EditText dateEt;
+    private TextView dateTv;
+    private Calendar now;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-	@Override
-	public void initParms(Bundle parms) {
-	}
+    @Override
+    public void initParms(Bundle parms) {
+        now = Calendar.getInstance();
+    }
 
-	@Override
-	public int bindLayout() {
-		return R.layout.activity_add_weight;
-	}
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_add_weight;
+    }
 
-	@Override
-	public void initView(View view) {
-		dateEt = (EditText) findViewById(R.id.addweather_dateet);
-//		dateEt.setEnabled(false);
-		dateEt.setOnClickListener(this);
-	}
+    @Override
+    public void initView(View view) {
+        dateTv = (TextView) findViewById(R.id.addweather_datetv);
+    }
 
-	@Override
-	public void doBusiness() {
+    @Override
+    public void doBusiness() {
+        findViewById(R.id.addweather_dateCv).setOnClickListener(this);
+        dateTv.setText(now.get(Calendar.YEAR) + "-" + now.get(Calendar.MONTH) + "-" + now.get(Calendar.DAY_OF_MONTH));
 
-	}
+    }
 
-	@Override
-	public void widgetClick(View v) {
-		switch (v.getId()) {
-			case R.id.addweather_dateet:
-				Calendar now = Calendar.getInstance();
-				DatePickerDialog dpd = DatePickerDialog.newInstance(
-						AddWeightActivity.this,
-						now.get(Calendar.YEAR),
-						now.get(Calendar.MONTH),
-						now.get(Calendar.DAY_OF_MONTH)
-				);
-				dpd.setVersion(DatePickerDialog.Version.VERSION_1);
-				dpd.show(getFragmentManager(), "Datepickerdialog");
-				break;
-		}
-	}
+    @Override
+    public void widgetClick(View v) {
+        switch (v.getId()) {
+            case R.id.addweather_dateCv:
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        AddWeightActivity.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.setVersion(DatePickerDialog.Version.VERSION_1);
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+                break;
+        }
+    }
 
-	@Override
-	public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-		dateEt.setText(year + "-" + monthOfYear + "-"+dayOfMonth);
-	}
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        dateTv.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
+    }
 }
